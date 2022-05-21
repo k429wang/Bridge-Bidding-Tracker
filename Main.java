96fbd258d5c834c78b1c5df6c377283f1d8d5cc2
Kai Wang <kdwang0917@gmail.com>
2022-05-21 13:34:21 -0400

    add basic commands/functionality

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String args[]) {
-        System.out.println("Test");
+        System.out.println(
+                "Welcome to Kai Wang's Bridge Bidding Tracker. Enter 'help' to view the commands, and 'quit' to terminate the program");
+
+        Scanner in = new Scanner(System.in);
+        Boolean quit = false;
+        while (quit == false) {
+            String command = in.nextLine();
+            switch (command) {
+                case "help":
+                    System.out.println("e");
+                    break;
+                case "quit":
+                    System.out.println("Thank you for using my program!");
+                    System.exit(0);
+                case "display":
+                    break;
+                case "add":
+                    break;
+                case "traverse":
+                    break;
+            }
+        }
+
     }
 }
